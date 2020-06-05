b703792e519d329eff092b0bb634771e7635729b
= <=>
2020-06-05 14:51:11 +0700

    pembuatan variabel theme kondisi berdasarkan hari

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,7 +21,8 @@ class Admin extends CI_Controller {
     public function index() {
         $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-
+        $kondisi=0;
+        //untuk percangan theme berdasarkan hari 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
