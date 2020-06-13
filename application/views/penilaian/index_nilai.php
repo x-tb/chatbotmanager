3ee4f1c064ad83a29eb22e4ee0b62007b848a5f4
= <=>
2020-06-14 00:16:36 +0700

    major modif view dan post method

--- application/views/penilaian/index_nilai.php
+++ application/views/penilaian/index_nilai.php
@@ -39,17 +39,17 @@
 			<!-- start -->
 			<table class="table table-bordered">
 				
-				<tr class="bg-secondary text-white">
+				<tr class="bg-light ">
 					<th colspan="5">
 						<h5>Kompetensi Dasar</h5>
 					</th>
 				</tr>
-                <tr>
+                <tr class="bg-dark text-white">
                     <th>No</th>
                     <th>Tipe Nilai</th>
                     <th>Nama Penugasan/Penilaian</th>
-                    <th>Aksi</th>
-                    <th>Info</th>
+                    <th>Upload</th>
+                    <th>Modify</th>
                 </tr>
 				<?php 
                             $i=0;
@@ -61,7 +61,7 @@
 
 
 				<tr>
-					<th colspan="5"><b><?php echo $mt->nomor_nama_kd; ?></b></th>
+					<th class="bg-light" colspan="5"><?php echo $mt->nomor_nama_kd; ?></th>
 
 				</tr>
 				<?php
@@ -78,19 +78,24 @@
 					<td></b>&nbsp;<?php echo $tg->nama_tugas; ?></td>
 
 					<td>
-						<a
-							href="<?php echo base_url("penugasan/upload_nilai/$tg->id_penugasan/$mt->id_materi/$mt->idguru/$mt->id_mapel"); ?>"><span
-								class="badge badge-success badge-pill"><i class="fa fa-upload" aria-hidden="true">
-									upload  &nbsp; </i>
-							</span></a>
-						<a
-							href="<?php echo base_url("penugasan/upload_nilai/$tg->id_penugasan/$mt->id_materi/$mt->idguru/$mt->id_mapel"); ?>"><span
-								class="badge badge-primary badge-pill"><i class="fa fa-eye" aria-hidden="true">
+					<form method="POST" action="<?=base_url("penilaian/upload_nilai")?>">
+					<input type="hidden" value="<?=$tg->id_penugasan?>" name="id_penugasan">
+					<input type="hidden" value="<?php echo $mp->nama_mapel; ?>" name="nama_mapel">
+					<input type="hidden" value="<?=$mt->nomor_nama_kd?>" name="nama_materi">
+					<input type="hidden" value="<?=$mt->idguru?>" name="id_guru">
+					<input type="hidden" value="<?=$mt->id_mapel?>" name="id_mapel">
+					<input type="hidden" value="<?=$mp->kode_mapel_ajar?>" name="kode_mapel">
+					<input type="hidden" value="<?=$tg->nama_tugas?>" name="nama_tugas">
+					<button class="btn btn-success btn-sm" type="submit"><i class="fa fa-upload" aria-hidden="true">
+									upload  &nbsp; </i></button>
+					</form>
+					</td>
+					<td>	<a
+							href="<?php echo base_url("penilaian/upload_nilai/$tg->id_penugasan/$mt->id_materi/$mt->idguru/$mt->id_mapel"); ?>"><span
+								class="btn btn-primary btn-sm"><i class="fa fa-eye" aria-hidden="true">
 									lihat/edit &nbsp; </i>
 							</span></a>
-
-					</td>
-					<td></td>
+</td>
 
 					
 				</tr>
@@ -108,19 +113,25 @@
 			</table>
 			<hr>
 			<?php endforeach; ?>
-
-
+<table class="table table-bordered">
+	<tr>
+		<th>Total Tagihan Nilai Akademik Anda </th>
+		<td><span id="totalTagihanNilai" class="badge badge-pill bg-primary text-white">Auto Jquery</span></td>
+		<th>Belum Terisi</th>
+		<td><span id="jumlahTerisi" class="badge badge-pill bg-danger text-white">5 item</span></td>
+		<th>Sudah Terisi</th>
+		<td><span id="jumlahTerisi" class="badge badge-pill bg-success text-white">3 item</span></td>
+		
+	</tr>
+</table>
+			
            
 			<!-- end -->
 		</div>
 
       
 	</div>
-    <div class="card-body col-md-12">
-            <ul class"col-md-12 list-group>
-                <li class="list-group-item d-flex justify-content-between align-items-center">Total Tagihan Nilai Anda : <span id="totalTagihanNilai" class="badge badge-pill bg-primary text-white">5</span></li>
-            </ul>        
-         </div>  
+   
 </div>
 <script>
 var numItems = $('.nomor').length;
