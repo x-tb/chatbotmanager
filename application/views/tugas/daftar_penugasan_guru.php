0b2d908ee8fe81e1e5aded58ae9b69cf6ab0b38c
= <=>
2020-06-14 20:13:36 +0700

    perubahan ke view tabel penugasan guru

--- application/views/tugas/daftar_penugasan_guru.php
+++ application/views/tugas/daftar_penugasan_guru.php
@@ -21,73 +21,98 @@
 		<object type="image/svg+xml" data="<?php echo base_url("assets/img/starbotsapa.svg"); ?>" width="400">
           Your browser does not support SVG.
         </object>
-		</div>
-			<?php 
-
-//print_r($tugas);
-
-
-?>
-
-			<!-- start -->
-			<div class="row">
-				<div class="card-group col-md-12">
-					<?php
+		<?php
 					
 					foreach($pelajaran as $mp): 
 					
 						?>
-					<div class="card col-md-6">
-						<img width="300" class="card-img-top"
-							src="<?php echo base_url('assets/img/newartboard2.png');?>" alt="">
-						<div class="card-body">
-							<h4 class="card-title"><?php echo $mp->nama_mapel; ?></h4>
-							<p><?php echo $mp->kode_mapel_ajar; ?></p>
+                        <tr>
+					<div class="card-header bg-primary col-md-12 text-white">
+						<h4><?php echo $mp->nama_mapel; ?></h4>
+						<p><?php echo $mp->kode_mapel_ajar; ?></p>
 
-						</div>
-						<div class="card-header bg-primary">
-							<h5 class="text-white">Kompetensi Dasar</h5>
-						</div>
-						<div class="list-group list-group-flush">
-							<?php 
+					</div>
+				</tr>
+			<!-- start -->
+			<table class="table table-bordered">
+				
+				<tr class="bg-light ">
+					<th colspan="5">
+						<h5>Kompetensi Dasar</h5>
+					</th>
+				</tr>
+                <tr class="bg-dark text-white">
+                    <th>No</th>
+                    <th>Tipe</th>
+                    <th>Nama Penugasan</th>
+                    <th>Pertemuan</th>
+                    <th>Modify</th>
+                </tr>
+				<?php 
                             $i=0;
                             foreach($materi as $mt):
                                 $i++;
                             if($mt->id_mapel==$mp->id_mapel){
 
                             ?>
-							<a href="<?php echo base_url("penugasan/form_penugasan_guru/$mt->id_materi/$mt->idguru/$mt->id_mapel");?>"
-								class="list-group-item list-group-item-action d-flex justify-content-between align-items-center ">
-								<b><?php echo $mt->nomor_nama_kd; ?></b><span class="badge badge-primary badge-pill"><i
-										class="fa fa-plus" aria-hidden="true"></i>
-								</span>
-							</a>
-							<ol class="list-group list-group-flush">
-							<?php foreach($tugas as $tg): 
+
+
+				<tr>
+					<th class="bg-light" colspan="3"><?php echo $mt->nomor_nama_kd; ?></th>
+					<th clas="text-center"><?=$mt->pertemuan_ke." s.d ".$mt->pertemuan_hingga?></th>
+					<th> <a class="btn btn-outline-primary btn-sm" href="<?php echo base_url("penugasan/form_penugasan_guru/$mt->id_materi/$mt->idguru/$mt->id_mapel");?>"> <i class="fa fa-plus" aria-hidden="true">
+									Tambah &nbsp; </i></a></th>
+
+				</tr>
+				<?php
+                //materi start looop
+                $notugas=0;
+                foreach($tugas as $tg): 
+                   
 								if($tg->id_materi==$mt->id_materi){
+                                    $notugas++;
 								?>
-								<li
-									class="list-group-item list-group-item-action d-flex justify-content-between align-items-left">
-									<p><b class="badge bg-primary text-white"><?php echo $tg->tipe_tugas; ?></b>&nbsp;<?php echo $tg->nama_tugas; ?></p>
-									<a href="<?php echo base_url("penugasan/form_edit_penugasan_guru/$tg->id_penugasan/$mt->id_materi/$mt->idguru/$mt->id_mapel"); ?>"><span class="badge badge-danger badge-pill"><i class="fa fa-edit"
-												aria-hidden="true"></i>
+				<tr class="nomor">
+					<td id="tugaske<?=$notugas?>"><?=$notugas?></td>
+					<td><?php echo $tg->tipe_tugas; ?></td>
+					<td></b>&nbsp;<?php echo $tg->nama_tugas; ?></td>
+
+					<td>
+					<?php echo $tg->tgl_penugasan; ?>
+					</td>
+					<td>	<a href="<?php echo base_url("penugasan/form_edit_penugasan_guru/$tg->id_penugasan/$mt->id_materi/$mt->idguru/$mt->id_mapel"); ?>"><span class="btn btn-outline-dark btn-sm"><i class="fa fa-edit"
+												aria-hidden="true"> Edit</i>
 										</span></a>
-								</li>
-							<?php 
+</td>
+
+					
+				</tr>
+				<?php 
+                //endloop for id materi content
 								}
-						endforeach; ?>       
-							</ol>
+						endforeach; ?>
 
-							<?php } ?>
-							<?php endforeach; ?>
-						</div>
-					</div>
-					<?php endforeach; ?>
-
-				</div>
-
-			</div>
-
+				<?php
+                //end loop mapel kd
+             } ?>
+				<?php 
+                        //end loop mapel
+                        endforeach; ?>
+			</table>
+			<hr>
+			<?php endforeach; ?>
+<table class="table table-bordered">
+	<tr>
+		<th>Total Tagihan Nilai Akademik Anda </th>
+		<td><span id="totalTagihanNilai" class="badge badge-pill bg-primary text-white">Auto Jquery</span></td>
+		<th>Belum Terisi</th>
+		<td><span id="jumlahTerisi" class="badge badge-pill bg-danger text-white">5 item</span></td>
+		<th>Sudah Terisi</th>
+		<td><span id="jumlahTerisi" class="badge badge-pill bg-success text-white">3 item</span></td>
+		
+	</tr>
+</table>
+			<?php //print_r($tugas); ?>
 
 
 			<!-- end -->
