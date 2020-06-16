8592bad4631be019914c456db11753207c212fe4
puguhrismadi <puguh.rismadi@gmail.com>
2020-06-16 17:07:58 +0700

    tambah tile smk taruna bhakti di viewnilai

--- application/views/penilaian/view_nilai.php
+++ application/views/penilaian/view_nilai.php
@@ -19,7 +19,7 @@
 
 	<div class="card shadow mb-4">
 		<div class="card-header py-3">
-			<h6 class="m-0 font-weight-bold text-primary">Rekap Nilai Mata Pelajaran </h6>
+			<h6 class="m-0 font-weight-bold text-primary">Rekap Nilai Mata Pelajaran di SMK Taruna Bhakti </h6>
 		</div>
 		<div class="card-body">
 
