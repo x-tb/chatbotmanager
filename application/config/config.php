a7213ca7ab9d2326761aa11191426a28ba5bf92b
puguhrismadi <puguhrismadi@gmail.com>
2020-07-08 18:42:31 +0700

    pindah ke server linux

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://7cbdb773abb6.ngrok.io/chatbotadmin';
+$config['base_url'] = 'http://localhost/chatbotmanager';
 
 /*
 |--------------------------------------------------------------------------
