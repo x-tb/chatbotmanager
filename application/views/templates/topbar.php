f8fe92af2d2e2e9c2ce72858bc33201a7845e741
= <=>
2020-07-14 22:00:47 +0700

    top bar dataclon

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -6,7 +6,7 @@
 
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
+         
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
@@ -14,7 +14,8 @@
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
+                       
+                    
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
