159c680c43dac273d8f3122c60a032c88eecd6bc
puguhrismadi <puguhrismadi@gmail.com>
2020-07-29 09:23:02 +0700

    add text dashboard admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,7 +20,7 @@ class Admin extends CI_Controller {
     }
 
     public function index() {
-        $data['title'] = 'Dashboard';
+        $data['title'] = 'Dashboard Admin';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $kondisi=0;
         //untuk percangan theme berdasarkan hari 
